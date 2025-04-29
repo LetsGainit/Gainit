4e197d50601bf06f4958964cb2b80eca63f28686
Gal Melman <165789349+galmelman@users.noreply.github.com>
2025-04-29 13:32:50 +0300

    Delete frontend directory
    moved to work in two repo's

--- frontend/src/components/FilterSearchProjectBar.jsx
+++
@@ -1,5 +0,0 @@
-function FilterSearchProjectBar() {
-
-  }
-  
-  export default FilterSearchProjectBar;
--- frontend/src/pages/ProjectOption.jsx
+++
@@ -1,7 +0,0 @@
-function ProjectOption1(){
-    return <div className="ProjectOption1">
-        
-    </div>
-}
-
-export default ProjectOption1
